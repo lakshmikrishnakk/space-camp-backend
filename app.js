f69e8734e0567dc44a3c2c4cf0643f5d27d23529
lakshmikrishnakk <lk2003kk@gmail.com>
2026-07-15 12:15:54 +0530

    view done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(
-    "mongodb://LakshmiKrishna\:lakshmikrishna\@ac-hpvbi7o-shard-00-00.fgcxtxc.mongodb.net:27017,ac-hpvbi7o-shard-00-01.fgcxtxc.mongodb.net:27017,ac-hpvbi7o-shard-00-02.fgcxtxc.mongodb.net:27017/campdb?ssl=true&replicaSet=atlas-14be5c-shard-0&authSource=admin&appName=Cluster0"
+    "mongodb://LakshmiKrishna:lakshmikrishna@ac-hpvbi7o-shard-00-00.fgcxtxc.mongodb.net:27017,ac-hpvbi7o-shard-00-01.fgcxtxc.mongodb.net:27017,ac-hpvbi7o-shard-00-02.fgcxtxc.mongodb.net:27017/campdb?ssl=true&replicaSet=atlas-14be5c-shard-0&authSource=admin&appName=Cluster0"
 )
     .then(() => {
         console.log("MongoDB Connected");
